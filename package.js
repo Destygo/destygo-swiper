fe28ef3abd8ac593be318badf7666ccd2a389570
Dmitry Shevchenko <dmitryvs@dreamteam.gg>
2019-02-21 19:03:38 +0200

    update repo links

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 var version = '4.4.6';
 
 Package.describe({
-  name: 'nolimits4web:swiper',
-  summary: 'iDangero.us Swiper - mobile touch slider with hardware accelerated transitions and native behavior',
+  name: 'thisninja:swiper',
+  summary: 'Swiper - mobile touch slider with hardware accelerated transitions and native behavior',
   version: version,
-  git: 'https://github.com/nolimits4web/Swiper'
+  git: 'https://github.com/thisninja/Swiper'
 });
 
 Package.onUse(function (api) {
